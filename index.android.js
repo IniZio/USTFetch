3e64aac0b8e92cb19c4f62f01f5867009ebf816b
inizio <digit4free@gmail.com>
2017-03-06 22:16:57 +0800

    react-native initialization

---
+++ index.android.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Button,
+  DrawerLayoutAndroid,
+  View
+} from 'react-native';
+
+export default class fetch extends Component {
+  render = () => {
+    var navigationView = (
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'center'}}>I'm in the Drawer!</Text>
+      </View>
+    );
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World?</Text>
+        </View>
+      </DrawerLayoutAndroid>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('fetch', () => fetch);
