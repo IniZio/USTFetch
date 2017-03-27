af36e38e1e663933c568d652ca235604976046a6
inizio <digit4free@gmail.com>
2017-03-27 18:37:40 +0800

    extract fake data from missionboard to const

--- Components/MissionBoard.js
+++ Components/MissionBoard.js
@@ -4,17 +4,29 @@ import {
   Content, Body,
   Card, CardItem,
   Button,
-  Icon, Text
+  Icon, Text,
+  List
 } from 'native-base'
 import {Avatar} from 'react-native-material-ui';
 
+const fakeMissions = [
+  { name: 'Uniqlo Flannel', deadline: '3 hours left', username: 'Yihao' },
+  { name: 'Uniqlo Flannel', deadline: '3 hours left', username: 'Yihao' },
+  { name: 'Uniqlo Flannel', deadline: '3 hours left', username: 'Yihao' },
+  { name: 'Uniqlo Flannel', deadline: '3 hours left', username: 'Yihao' },
+  { name: 'Uniqlo Flannel', deadline: '3 hours left', username: 'Yihao' }
+]
+
 export default class MissionBoard extends Component {
+  state = {
+    missions: fakeMissions
+  }
   render = () => {
     const { navigate } = this.props.navigation
     return (
       <Content>
-        {[,...Array(5)].map((x, index) => (
-          <Card key={index}>
+        <List dataArray={this.state.missions} renderRow={mission => (
+          <Card>
             <CardItem onPress={() => navigate('MissionDetail')}>
               <Body>
                 <View style={{ flexDirection: 'row', flex: 1 }}>
@@ -22,14 +34,14 @@ export default class MissionBoard extends Component {
                   <View style={{justifyContent: 'center', alignItems: 'center'}}><Image source={require('../Fetch.png')} /></View>
                   {/*Align the product attributes*/}
                   <View style={{ flexDirection: 'column', flex: 1, padding: 10 }}>
-                    <Text style={{ fontSize: 20 }}>Uniqlo Flannel</Text>
-                    <Text>3 hours left</Text>
+                    <Text style={{ fontSize: 20 }}>{mission.name}</Text>
+                    <Text>{mission.deadline}</Text>
                     <View style={{ flexDirection: 'row', flex: 1, paddingVertical: 5 }}>
                       <View style={{ height: 50, width: 55 }}>
-                        <Avatar text="E" size={40} />
+                        <Avatar text={mission.username[0]} size={40} />
                       </View>
                       <View style={{ flexDirection: 'column', flex: 1 }}>
-                        <Text>Edmund Pan</Text>
+                        <Text>{mission.username}</Text>
                         <View style={{ flexDirection: 'row', flex: 1 }}>
                           {[,...Array(5)].map((x, index) => (
                             <Icon name="star" style={{ fontSize: 20 }} key={index}/>
@@ -47,7 +59,7 @@ export default class MissionBoard extends Component {
               </Body>
             </CardItem>
           </Card>
-        ))}
+        )} />
       </Content>
     )
   }
