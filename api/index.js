4830dde428ee15f6b6bafff9edb3810fe20c548a
inizio <digit4free@gmail.com>
2017-04-24 23:26:01 +0800

    socket url define

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import io from 'socket.io-client'
 
-// const URL = 'http://192.168.31.144:8080'
-const URL = 'http://10.89.80.125:8080'
+const SERVER_URL = 'http://192.168.31.144'
+
+export const SOCKET_URL = `${SERVER_URL}:8081`
+const API_URL = `${SERVER_URL}:8080`
 
 // Task
 export function fetchTasks () {
-  return fetch(`${URL}/task`)
+  return fetch(`${API_URL}/task`)
     .then(response => response.json())
     .catch(err => console.error(err))
 }
 
 export function createTask (task) {
-  return fetch(`${URL}/task`, {
+  return fetch(`${API_URL}/task`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -24,7 +27,7 @@ export function createTask (task) {
 }
 
 export function updateTask (task) {
-  return fetch(`${URL}/task`, {
+  return fetch(`${API_URL}/task`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
